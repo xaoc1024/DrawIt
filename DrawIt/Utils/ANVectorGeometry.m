90bd86889e1858df9e8c02c33b24fc391a76d658
Serdyuk Serhiy <stargazer1342@gmail.com>
2013-12-17 05:30:09 -0800

    circle rasterisation

--- DrawIt/Utils/ANVectorGeometry.m
+++ DrawIt/Utils/ANVectorGeometry.m
@@ -10,16 +10,74 @@
 
 
 CGPoint * pointsArrayForCicle(CGPoint center, float radius, NSInteger *pointsNumber) {
-    NSUInteger pointsCount = 50; // must be calculated according to cicle size
+    
+    float x = 0;
+    float y = radius;
+    float delta = 1 - 2 * radius;
+    float error = 0;
+    int count = 0;
+    
+    while (y >= 0) {
+    
+        ++count;
+        
+        error = 2 * (delta + y) - 1;
+		if (delta < 0 && error <= 0) {
+            ++x;
+            delta += 2 * x + 1;
+            continue;
+        }
+        
+		error = 2 * (delta - x) - 1;
+		if (delta > 0 && error > 0) {
+			--y;
+			delta += 1 - 2 * y;
+			continue;
+        }
+        
+        ++x;
+		delta += 2 * (x - y);
+		--y;
+    }
+    
+    NSUInteger pointsCount = 4 * count;
     *pointsNumber = pointsCount;
     
     CGPoint * pointArray = calloc(pointsCount, sizeof(CGPoint));
     
-    // here must be written your code :)
-    for (int i = 0; i < pointsCount; i++){
-        pointArray[i] = CGPointMake(i, 1.2 * i);
+    x = 0;
+    y = radius;
+    delta = 1 - 2 * radius;
+    error = 0;
+    int i = 0;
+
+    while (y >= 0) {
+        
+        pointArray[i] = CGPointMake(center.x + x, center.y + y);
+        pointArray[i + 1] = CGPointMake(center.x + x, center.y - y);
+        pointArray[i + 2] = CGPointMake(center.x - x, center.y + y);
+        pointArray[i + 3] = CGPointMake(center.x - x, center.y - y);
+        
+        i += 4;
+        
+        error = 2 * (delta + y) - 1;
+		if (delta < 0 && error <= 0) {
+            ++x;
+            delta += 2 * x + 1;
+            continue;
+        }
+        
+		error = 2 * (delta - x) - 1;
+		if (delta > 0 && error > 0) {
+			--y;
+			delta += 1 - 2 * y;
+			continue;
+        }
+        
+        ++x;
+		delta += 2 * (x - y);
+		--y;
     }
-    
     
     // log section
     NSMutableString * logString = [NSMutableString stringWithString:@"LOG for pints:\n"];
